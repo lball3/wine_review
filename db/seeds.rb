0bfe48a20b9c6e6452b69a849c412a2d93f96fe4
lball3 <louis.ball@me.com>
2015-12-19 08:29:02 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do
 
-Wine.create(name: 'Riesling',year: 1990, winery: 'Old Vine of riesling',country: 'Germany', varietal: 'lighter than Chardonnay')
-Wine.create(name: 'Chardonnay',year: 1990, winery: 'Chardonnay vine',country: 'France', varietal: 'wider-bodied than other dry wines')
-Wine.create(name: 'Sauvignon Blanc',year: 2000, winery: 'Sauvignon blanc',country: 'Australian', varietal: 'lighter than Chardonnay')
-Wine.create(name: 'Syrah',year: 1980, winery: 'Syrah vine',country: 'France', varietal: 'hearty, spicy red')
-Wine.create(name: 'Merlot',year: 1970, winery: 'Merlot vine',country: 'U.S.', varietal: 'black-cherry and herbal flavours')
-Wine.create(name: 'Cabernet Sauvignon',year: 1965, winery: 'Cabernet sauvignon',country: 'Germany', varietal: 'full-bodied, but firm')
+Wine.create([
+{ name: 'Riesling',year: 1990, winery: 'Old Vine of riesling',country: 'Germany', varietal: 'lighter than Chardonnay'},
+{ name: 'Chardonnay',year: 1990, winery: 'Chardonnay vine',country: 'France', varietal: 'wider-bodied than other dry wines'},
+{ name: 'Sauvignon Blanc',year: 2000, winery: 'Sauvignon blanc',country: 'Australian', varietal: 'lighter than Chardonnay'},
+{ name: 'Syrah',year: 1980, winery: 'Syrah vine',country: 'France', varietal: 'hearty, spicy red'},
+{ name: 'Merlot',year: 1970, winery: 'Merlot vine',country: 'U.S.', varietal: 'black-cherry and herbal flavours'},
+{ name: 'Cabernet Sauvignon',year: 1965, winery: 'Cabernet sauvignon',country: 'Germany', varietal: 'full-bodied, but firm'}
+  ])
+  
+end 
