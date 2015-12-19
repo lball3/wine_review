213f62882a464943fdb7a8afda3d096a22a4bb47
Louis Ball <louis.ball@me.com>
2015-12-19 10:58:41 -0800

    update model Wine

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,12 @@
 class Wine < ActiveRecord::Base
+
+VARIETALS = ['Riesling', 'Chardonnay', 'Sauvignon Blanc', 'Syrah', 'Merlot',
+              'Cabernet Sauvignon']
+
+  validates :name, :country, :year, presence: :true
+  validates :year, numericality: { only_integer: true }
+  validates :varietal, inclusion: { in: VARIETALS }
+
+  paginates_per 5
+
 end
