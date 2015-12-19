b62284ce8e6842f4196027bc2a089496665b3353
Louis Ball <louis.ball@me.com>
2015-12-19 06:16:43 -0800

    commit change

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
 
   resources :wines do
-    resources :log_entries
+
   end
 
-  resources :log_entries, only: [:show, :index]
 
-  root 'wines#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
