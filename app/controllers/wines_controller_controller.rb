b4054bdcb8d47983fd073d0e320d80e063331c32
Louis Ball <louis.ball@me.com>
2015-12-19 11:30:18 -0800

    update to wine_controller

--- app/controllers/wines_controller_controller.rb
+++ app/controllers/wines_controller_controller.rb
@@ -1,4 +1,6 @@
 class WinesControllerController < ApplicationController
+    before_action :set_wine, only: [:show, :edit, :update, :destroy]
+
   def index
       @wines = Wine.find(:all)
       
@@ -13,4 +15,47 @@ class WinesControllerController < ApplicationController
       respond_to do |format|
           format.html #index.html.erb
       end
+   end
+
+   def new
+    @wine = Wine.new
+   end
+
+   def edit
+
+   end
+
+   def create
+    @wine = Wine.new(wine_params)
+    if @wine.save
+      flash[:success] = 'Wine was successfully created.'
+      redirect_to @wine
+    else
+      render :new
     end
+   end
+
+   def update
+    if @wine.update(wine_params)
+      flash[:success] = 'Wine was successfully updated.'
+      redirect_to @wine
+    else
+      render :edit
+    end
+    end
+
+   def destroy
+    @wine.destroy
+    flash[:success] = 'Wine was successfully deleted.'
+    redirect_to wines_url
+    end
+
+  private
+    def set_wine
+      @wine = Wine.find(params[:id])
+    end
+
+    def wine_params
+      params.require(:wine).permit(:name, :year, :winery, :country, :varietal)
+    end
+ end
